cca4c0a43e9009e433ff36d0edcd7217e3d4e8ff
AmirGholizadeh <amir110gholizadeh@gmail.com>
2021-02-10 17:54:26 +0330

    feat: connect to database and listen to port

--- server.js
+++ server.js
@@ -1,3 +1,18 @@
+require('dotenv').config({path:"./config.env"});
 const mongoose = require('mongoose');
 const app = require('./app');
 
+const mongoUrl = process.env.MONGO_URL;
+const port = process.env.PORT;
+
+mongoose.connect(mongoUrl, {
+    useFindAndModify:true,
+    useUnifiedTopology:true,
+    useNewUrlParser:true,
+}, () => {
+    console.info('connected to the database');
+});
+
+app.listen(port, () => {
+    console.info(`connected to port ${port}`);
+});

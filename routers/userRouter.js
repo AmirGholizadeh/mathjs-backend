13f678982cc7dc2f96c67d6dade4812cf16bf37e
AmirGholizadeh <amir110gholizadeh@gmail.com>
2021-02-11 14:18:43 +0330

    feat: add a route to get top users

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -7,6 +7,8 @@ const router = express.Router();
 router.post('/signup', authController.signup);
 router.post('/login', authController.login)
 router.get('/validateToken/:token', authController.validateToken);
+router.post('/editTopScore', authController.protect, userController.editTopScore);
+router.get('/getTopUsers/:page', userController.getTopUsers);
 router.use(authController.restrict(['manager', 'admin']))
 router.route('/').get(userController.getAllUsers);
 router.route('/:id').get(userController.getOneUser);

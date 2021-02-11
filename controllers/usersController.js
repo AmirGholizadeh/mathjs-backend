a999397ad1a064a5d81bbb0c9fb6ddeacb0c1bde
AmirGholizadeh <amir110gholizadeh@gmail.com>
2021-02-11 14:18:02 +0330

    feat: add a controller to get top users

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,7 +1,38 @@
 const User = require('../models/userModel');
+const jwt = require('jsonwebtoken');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError')
 const handlerFactory = require('./handlerFactory');
 
 exports.getAllUsers = handlerFactory.getAll(User);
 exports.getOneUser = handlerFactory.getOne(User);
+
+exports.getTopUsers = catchAsync(async(req,res,next) => {
+    const users = await User.aggregate([{
+        $skip:10 * req.params.page},
+        {$limit:10},
+        {$sort:{topScore:1}
+    }]);
+    res.status(200).json({
+        results:users.length,
+        data:{
+            users
+        }
+    });
+});
+
+exports.editTopScore = catchAsync(async(req,res,next) => {
+    const {topScore} = req.body;
+    const {token} = req.query;
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await User.findById(decoded.id);
+    user.topScore = topScore;
+    user.save({runValidators:false});
+    res.status(200).json({
+        status:'ok',
+        message:'user topscore is updated',
+        data:{
+            user
+        }
+    });
+});

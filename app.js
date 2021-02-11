045ff0835e60ed50504fce1c8c60e4a44d0527c5
AmirGholizadeh <amir110gholizadeh@gmail.com>
2021-02-11 15:22:02 +0330

    feat: add router for tickets

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const helmet = require('helmet');
 
 const userRouter = require('./routers/userRouter');
+const ticketRouter = require('./routers/ticketRouter');
 const errorController = require('./controllers/errorController');
 
 const app = express();
@@ -11,6 +12,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(helmet());
 
 app.use('/api/v1/users', userRouter);
+app.use('/api/v1/tickets', ticketRouter);
 
 app.use('*', (req,res,next) => {
     console.log('the page is not found!');

3b851d1aae4ffd2968d481c46e4da64b939e744a
AmirGholizadeh <amir110gholizadeh@gmail.com>
2021-02-11 13:05:10 +0330

    feat: add get users routes and restrict them

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const authController = require('../controllers/authController');
+const userController = require('../controllers/usersController');
 
 const router = express.Router();
 
 router.post('/signup', authController.signup);
 router.post('/login', authController.login)
+router.use(authController.restrict(['manager', 'admin']))
+router.route('/').get(userController.getAllUsers);
+router.route('/:id').get(userController.getOneUser);
 
 module.exports = router;
